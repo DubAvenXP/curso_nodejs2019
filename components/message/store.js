effcc5573d6c4854e38900f92cbc0110b62db61b
Alejandro Dubon <alejandrodubon88@gmail.com>
2020-12-26 15:27:24 -0600

    primera conexion exitosa con la db

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,31 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model')
+
+const user = 'db_user_nodejs';
+const password = 'RIOifQx9zXbhe7Pt';
+const database = 'cursonodejs';
+const uri = `mongodb+srv://${user}:${password}@cluster0.pjeso.mongodb.net/${database}?retryWrites=true&w=majority`
+
+//connection 
+db.Promise = global.Promise;
+db.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log('[db] conectada con exito');
+})
+.catch((e => {
+    console.error('hubo un error con la conexion a la DB');
+    console.error(e);
+}));
+
 
 function addMessage(message) {
-    list.push(message)
+    const myMessage = new Model(message)
+    myMessage.save();
 }
 
-function getMessage() {
-    return list;
+async function getMessage() {
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
